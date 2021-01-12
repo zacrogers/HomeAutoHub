ec045034928797cbcef6241f293ac03d64dcb1d0
rogerszach <z.m.rogers@gmail.com>
2021-01-12 22:41:53 +1300

    Add project files.

---
+++ HomeAutoHub.Shared/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeAutoHub.Shared
+{
+    class Class1
+    {
+    }
+}
---
+++ HomeAutoHub.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace HomeAutoHub.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new HomeAutoHub.App(), args);
+            host.Run();
+        }
+    }
+}
